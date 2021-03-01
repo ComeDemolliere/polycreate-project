72f2cca49e769f1d97ac28e68e47515163321545
AlexiaGIRARD <alexia.girard@etu.unice.fr>
2021-03-01 16:13:40 +0100

    added conditions on transitions arrows

--- fr.unice.polytech.si5.webots.polycreate.design/src/fr/unice/polytech/si5/webots/polycreate/design/Services.java
+++ fr.unice.polytech.si5.webots.polycreate.design/src/fr/unice/polytech/si5/webots/polycreate/design/Services.java
@@ -2,6 +2,17 @@ package fr.unice.polytech.si5.webots.polycreate.design;
 
 import org.eclipse.emf.ecore.EObject;
 
+import fr.unice.polytech.si5.webots.polycreate.abstractsyntax.polycreate.AngleCondition;
+import fr.unice.polytech.si5.webots.polycreate.abstractsyntax.polycreate.Condition;
+import fr.unice.polytech.si5.webots.polycreate.abstractsyntax.polycreate.DistanceCondition;
+import fr.unice.polytech.si5.webots.polycreate.abstractsyntax.polycreate.GRIPPER_STATE;
+import fr.unice.polytech.si5.webots.polycreate.abstractsyntax.polycreate.GripAction;
+import fr.unice.polytech.si5.webots.polycreate.abstractsyntax.polycreate.MoveAction;
+import fr.unice.polytech.si5.webots.polycreate.abstractsyntax.polycreate.OPERATOR;
+import fr.unice.polytech.si5.webots.polycreate.abstractsyntax.polycreate.SimpleCondition;
+import fr.unice.polytech.si5.webots.polycreate.abstractsyntax.polycreate.Transition;
+import fr.unice.polytech.si5.webots.polycreate.abstractsyntax.polycreate.TurnAction;
+
 /**
  * The services class used by VSM.
  */
@@ -13,5 +24,53 @@ public class Services {
     public EObject myService(EObject self, String arg) {
        // TODO Auto-generated code
       return self;
+    }
+    
+    public String printTransition(Transition transition) {
+    	String res = "";
+    	for (Condition condition : transition.getConditions()) {
+    		if (condition instanceof SimpleCondition) {
+    			res += "there is a " + ((SimpleCondition) condition).getDetectionType().toString() + "\n";
+    		}
+    		if (condition instanceof AngleCondition) {
+    			res += "angle ";
+    			res += ((AngleCondition) condition).getOperator().equals(OPERATOR.INFERIOR) ? "< " : "> ";
+    			res += ((AngleCondition) condition).getAngle();
+    			res += " on camera " + ((AngleCondition) condition).getCameraType() + "\n";
+    		}
+    		if (condition instanceof DistanceCondition) {
+    			res += " distance ";
+    			res += ((DistanceCondition) condition).getOperator().equals(OPERATOR.INFERIOR) ? "< " : "> ";
+    			res += ((DistanceCondition) condition).getDistance();
+    			res += " on camera " + ((DistanceCondition) condition).getCameraType() + "\n";
+    		}
+			
+		}
+    	if (res.isEmpty()) {
+    		return "No conditions";
+    	}
+    	return res;
+    }
+    
+    public String printMoveAction(MoveAction action) {
+    	String res = "Move " + action.getDirection().toString();
+    	if (action.getDuration() > 0) {
+    		res += " during " + action.getDuration() + " secondes";
+    	}
+    	
+    	return res;
+    }
+    
+    public String printTurnAction(TurnAction action) {
+    	String res = "Turn " + action.getAngle() + "°";
+    	if (action.getDuration() > 0) {
+    		res += " during " + action.getDuration() + " secondes";
+    	}
+    	
+    	return res;
+    }
+
+    public String printGripAction(GripAction action) {
+    	return action.getState().equals(GRIPPER_STATE.OPEN) ? "Open gripper " : "Close gripper";    	
     }
 }
